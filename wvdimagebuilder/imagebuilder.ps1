0a3a4a07d3488987dd19c58451ee5b5a7cea0567
Mike Rodrick <mike@itpro.tv>
2020-06-30 13:16:57 -0400

    Create imagebuilder.ps1
    Create a Windows VM with Azure Image Builder using PowerShell

---
+++ wvdimagebuilder/imagebuilder.ps1
@@ -0,0 +1,157 @@
+# Install Azure PowerShell module
+if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
+    Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
+      'Az modules installed at the same time is not supported.')
+} else {
+    Install-Module -Name Az -AllowClobber -Scope CurrentUser
+}
+
+# While the Az.ImageBuilder and Az.ManagedServiceIdentity PowerShell modules are in preview, you must 
+# install them separately using the Install-Module cmdlet with the AllowPrerelease parameter
+'Az.ImageBuilder', 'Az.ManagedServiceIdentity' | ForEach-Object {Install-Module -Name $_ -AllowPrerelease}
+
+# First time, must register providers
+Register-AzProviderFeature -ProviderNamespace Microsoft.VirtualMachineImages -FeatureName VirtualMachineTemplatePreview
+
+# Wait until registration completes, takes several minutes
+Get-AzProviderFeature -ProviderNamespace Microsoft.VirtualMachineImages -FeatureName VirtualMachineTemplatePreview
+
+# Register additional providers if necessary
+Get-AzResourceProvider -ProviderNamespace Microsoft.Compute, Microsoft.KeyVault, Microsoft.Storage, Microsoft.VirtualMachineImages |
+  Where-Object RegistrationState -ne Registered |
+    Register-AzResourceProvider
+
+# Define variables for information that will be used repeatedly
+# Destination image resource group name
+$imageResourceGroup = 'rg-ImageBuilderTest'
+
+# Azure region
+$location = 'EastUS'
+
+# Name of the image to be created
+$imageTemplateName = 'wvdImage01'
+
+# Distribution properties of the managed image upon completion
+$runOutputName = 'myDistResults'
+
+# Your Azure Subscription ID
+$subscriptionID = (Get-AzContext).Subscription.Id
+Write-Output $subscriptionID
+
+# Create a resource group
+New-AzResourceGroup -Name $imageResourceGroup -Location $location
+
+# Create user identity and set role permissions
+# Create variables
+[int]$timeInt = $(Get-Date -UFormat '%s')
+$imageRoleDefName = "Azure Image Builder Image Def $timeInt"
+$identityName = "myIdentity$timeInt"
+
+# Create user identity
+New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName
+
+# Store the identity resource and principal IDs in variables
+$identityNameResourceId = (Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id
+$identityNamePrincipalId = (Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
+
+# Assign permissions for identity to distribute images
+$myRoleImageCreationUrl = 'https://raw.githubusercontent.com/danielsollondon/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json'
+$myRoleImageCreationPath = "$env:TEMP\myRoleImageCreation.json"
+
+Invoke-WebRequest -Uri $myRoleImageCreationUrl -OutFile $myRoleImageCreationPath -UseBasicParsing
+
+$Content = Get-Content -Path $myRoleImageCreationPath -Raw
+$Content = $Content -replace '<subscriptionID>', $subscriptionID
+$Content = $Content -replace '<rgName>', $imageResourceGroup
+$Content = $Content -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName
+$Content | Out-File -FilePath $myRoleImageCreationPath -Force
+
+# Create the role definition
+New-AzRoleDefinition -InputFile $myRoleImageCreationPath
+
+# Grant the role definition to the image builder service principal
+$RoleAssignParams = @{
+  ObjectId = $identityNamePrincipalId
+  RoleDefinitionName = $imageRoleDefName
+  Scope = "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+}
+New-AzRoleAssignment @RoleAssignParams
+
+# Create a Shared Image Gallery
+# Create the gallery
+$myGalleryName = 'myImageGallery'
+$imageDefName = 'winSvrImages'
+
+New-AzGallery -GalleryName $myGalleryName -ResourceGroupName $imageResourceGroup -Location $location
+
+# Create a gallery definition
+$GalleryParams = @{
+  GalleryName = $myGalleryName
+  ResourceGroupName = $imageResourceGroup
+  Location = $location
+  Name = $imageDefName
+  OsState = 'generalized'
+  OsType = 'Windows'
+  Publisher = 'myCo'
+  Offer = 'Windows'
+  Sku = 'Win2019'
+}
+New-AzGalleryImageDefinition @GalleryParams
+
+# Create an image
+# Create an Azure image builder source object
+$SrcObjParams = @{
+  SourceTypePlatformImage = $true
+  Publisher = 'MicrosoftWindowsServer'
+  Offer = 'WindowsServer'
+  Sku = '2019-Datacenter'
+  Version = 'latest'
+}
+$srcPlatform = New-AzImageBuilderSourceObject @SrcObjParams
+
+# Create an Azure image builder distributor object
+$disObjParams = @{
+  SharedImageDistributor = $true
+  ArtifactTag = @{tag='dis-share'}
+  GalleryImageId = "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup/providers/Microsoft.Compute/galleries/$myGalleryName/images/$imageDefName"
+  ReplicationRegion = $location
+  RunOutputName = $runOutputName
+  ExcludeFromLatest = $false
+}
+$disSharedImg = New-AzImageBuilderDistributorObject @disObjParams
+
+# Create an Azure image builder customization object
+$ImgCustomParams = @{
+  PowerShellCustomizer = $true
+  CustomizerName = 'settingUpMgmtAgtPath'
+  RunElevated = $false
+  Inline = @("mkdir c:\\buildActions", "echo Azure-Image-Builder-Was-Here  > c:\\buildActions\\buildActionsOutput.txt")
+}
+$Customizer = New-AzImageBuilderCustomizerObject @ImgCustomParams
+
+# Create an Azure image builder template
+$ImgTemplateParams = @{
+  ImageTemplateName = $imageTemplateName
+  ResourceGroupName = $imageResourceGroup
+  Source = $srcPlatform
+  Distribute = $disSharedImg
+  Customize = $Customizer
+  Location = $location
+  UserAssignedIdentityId = $identityNameResourceId
+}
+New-AzImageBuilderTemplate @ImgTemplateParams
+
+# Verify successful template creation
+Get-AzImageBuilderTemplate -ImageTemplateName $imageTemplateName -ResourceGroupName $imageResourceGroup |
+  Select-Object -Property Name, LastRunStatusRunState, LastRunStatusMessage, ProvisioningState
+
+# Start the image build
+Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName
+
+# Store credentials
+$cred = Get-Credential
+
+# Create a VM
+$ArtifactId = (Get-AzImageBuilderRunOutput -ImageTemplateName $imageTemplateName -ResourceGroupName $imageResourceGroup).ArtifactId
+
+New-AzVM -ResourceGroupName $imageResourceGroup -Image $ArtifactId -Name myWinVM01 -Credential $Cred
