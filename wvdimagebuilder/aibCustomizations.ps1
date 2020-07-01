a926434fe3bcf39c2f2fc629bbec4fd558251536
Mike Rodrick <mike@itpro.tv>
2020-07-01 10:23:50 -0400

    Create aibCustomizations.ps1
    Testing customization of AIB

---
+++ wvdimagebuilder/aibCustomizations.ps1
@@ -0,0 +1,20 @@
+#Script to setup golden image with Azure Image Builder
+
+
+#Create temp folder
+New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
+
+
+#Install VSCode
+Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile 'c:\temp\VScode.exe'
+Invoke-Expression -Command 'c:\temp\VScode.exe /verysilent'
+
+#Start sleep
+Start-Sleep -Seconds 10
+
+#InstallNotepadplusplus
+Invoke-WebRequest -Uri 'https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.x64.exe' -OutFile 'c:\temp\notepadplusplus.exe'
+Invoke-Expression -Command 'c:\temp\notepadplusplus.exe /S'
+
+#Start sleep
+Start-Sleep -Seconds 10
