837d75f6aae0a86cb8e50a9c17e52a11f5a9a046
Mike Rodrick <mike@itpro.tv>
2020-07-10 10:06:24 -0400

    Update endlessLove.ps1
    added out-null

--- david-zira-duet/endlessLove.ps1
+++ david-zira-duet/endlessLove.ps1
@@ -28,88 +28,88 @@ $zira.Speak("You're every step I make")
 $david.speak("And I")
 $zira.speak("iei")
 $david.speak("I want to share")
-$david.speakasync("All my love with you")
-$zira.speakasync("All my love with you")
+$david.speakasync("All my love with you") | Out-Null
+$zira.speakasync("All my love with you") | Out-Null
 $david.speak("No one else will do")
 
 $zira.speak("And your eyes")
 $david.speak("Your eyes, your eyes")
-$david.speakasync("They tell me how much you care")
-$zira.speakasync("They tell me how much you care")
-$david.speakasync("Ooh yes, you will always be")
-$zira.speakasync("Ooh yes, you will always be")
-$david.speakasync("My")
-$zira.speakasync("My")
-$david.speakasync("Endless love")
-$zira.speakasync("Endless love")
+$david.speakasync("They tell me how much you care") | Out-Null
+$zira.speakasync("They tell me how much you care") | Out-Null
+$david.speakasync("Ooh yes, you will always be") | Out-Null
+$zira.speakasync("Ooh yes, you will always be") | Out-Null
+$david.speakasync("My") | Out-Null
+$zira.speakasync("My") | Out-Null
+$david.speakasync("Endless love") | Out-Null
+$zira.speakasync("Endless love") | Out-Null
 
-$david.speakasync("Two hearts")
-$zira.speakasync("Two hearts")
-$david.speakasync("Two hearts that beat as one")
-$zira.speakasync("Two hearts that beat as one")
-$david.speakasync("Our lives have just begun")
-$zira.speakasync("Our lives have just begun")
+$david.speakasync("Two hearts") | Out-Null
+$zira.speakasync("Two hearts") | Out-Null
+$david.speakasync("Two hearts that beat as one") | Out-Null
+$zira.speakasync("Two hearts that beat as one") | Out-Null
+$david.speakasync("Our lives have just begun") | Out-Null
+$zira.speakasync("Our lives have just begun") | Out-Null
 
 $zira.speak("Forever")
 $david.speak("Oh")
-$david.speakasync("I'll hold you close in my arms")
-$zira.speakasync("I'll hold you close in my arms")
-$david.speakasync("I can't resist your charms")
-$zira.speakasync("I can't resist your charms")
+$david.speakasync("I'll hold you close in my arms") | Out-Null
+$zira.speakasync("I'll hold you close in my arms") | Out-Null
+$david.speakasync("I can't resist your charms") | Out-Null
+$zira.speakasync("I can't resist your charms") | Out-Null
 
 $zira.speak("And love")
 $david.speak("Oh, love")
-$david.speakasync("I'll be a fool")
-$zira.speakasync("I'll be a fool")
-$david.speakasync("For you")
-$zira.speakasync("For you")
-$david.speakasync("I'm sure")
-$zira.speakasync("I'm sure")
+$david.speakasync("I'll be a fool") | Out-Null
+$zira.speakasync("I'll be a fool") | Out-Null
+$david.speakasync("For you") | Out-Null
+$zira.speakasync("For you") | Out-Null
+$david.speakasync("I'm sure") | Out-Null
+$zira.speakasync("I'm sure") | Out-Null
 $zira.speak("You know I don't mind")
 $david.speak("Oh, you know I don't mind")
 
-$david.speakasync("Cause you")
-$zira.speakasync("Cause you")
-$david.speakasync("You mean the world to me")
-$zira.speakasync("You mean the world to me")
-$david.speakasync("Oh")
-$zira.speakasync("Oh")
+$david.speakasync("Cause you") | Out-Null
+$zira.speakasync("Cause you") | Out-Null
+$david.speakasync("You mean the world to me") | Out-Null
+$zira.speakasync("You mean the world to me") | Out-Null
+$david.speakasync("Oh") | Out-Null
+$zira.speakasync("Oh") | Out-Null
 $zira.speak("I know")
 $david.speak("I know")
-$david.speakasync("I've found in you")
-$zira.speakasync("I've found in you")
-$david.speakasync("My")
-$zira.speakasync("My")
-$david.speakasync("Endless love")
-$zira.speakasync("Endless love")
+$david.speakasync("I've found in you") | Out-Null
+$zira.speakasync("I've found in you") | Out-Null
+$david.speakasync("My") | Out-Null
+$zira.speakasync("My") | Out-Null
+$david.speakasync("Endless love") | Out-Null
+$zira.speakasync("Endless love") | Out-Null
 
-$david.speakasync("Oh, and love")
-$zira.speakasync("Oh, and love")
+$david.speakasync("Oh, and love") | Out-Null
+$zira.speakasync("Oh, and love") | Out-Null
 $david.speak("Oh, love")
-$david.speakasync("I'll be that fool")
-$zira.speakasync("I'll be that fool")
-$david.speakasync("For you")
-$zira.speakasync("For you")
-$david.speakasync("I'm sure")
-$zira.speakasync("I'm sure")
+$david.speakasync("I'll be that fool") | Out-Null
+$zira.speakasync("I'll be that fool") | Out-Null
+$david.speakasync("For you") | Out-Null
+$zira.speakasync("For you") | Out-Null
+$david.speakasync("I'm sure") | Out-Null
+$zira.speakasync("I'm sure") | Out-Null
 $zira.speak("You know I don't mind")
 $david.speak("Oh you know")
-$david.speakasync("I don't mind")
-$zira.speakasync("I don't mind")
+$david.speakasync("I don't mind") | Out-Null
+$zira.speakasync("I don't mind") | Out-Null
 
-$david.speakasync("And, yes")
-$zira.speakasync("And, yes")
-$david.speakasync("You'll be the only one")
-$zira.speakasync("You'll be the only one")
-$david.speakasync("Cause no one can deny")
-$zira.speakasync("Cause no one can deny")
-$david.speakasync("This love I have inside")
-$zira.speakasync("This love I have inside")
-$david.speakasync("And I'll give it all to you")
-$zira.speakasync("And I'll give it all to you")
+$david.speakasync("And, yes") | Out-Null
+$zira.speakasync("And, yes") | Out-Null
+$david.speakasync("You'll be the only one") | Out-Null
+$zira.speakasync("You'll be the only one") | Out-Null
+$david.speakasync("Cause no one can deny") | Out-Null
+$zira.speakasync("Cause no one can deny") | Out-Null
+$david.speakasync("This love I have inside") | Out-Null
+$zira.speakasync("This love I have inside") | Out-Null
+$david.speakasync("And I'll give it all to you") | Out-Null
+$zira.speakasync("And I'll give it all to you") | Out-Null
 $david.speak("My love")
 $zira.speak("My love, my love")
-$david.speakasync("My")
-$zira.speakasync("My")
-$david.speakasync("Endless love")
-$zira.speakasync("Endless love")
+$david.speakasync("My") | Out-Null
+$zira.speakasync("My") | Out-Null
+$david.speakasync("Endless love") | Out-Null
+$zira.speakasync("Endless love") | Out-Null
